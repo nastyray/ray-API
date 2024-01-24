368e713c8d31ce66c74f270c79141464131e858f
ray <2313715475@qq.com>
2024-01-24 17:22:36 +0800

    后端初始化
    简单实现接口的增删改查

---
+++ rayapi-backend/src/main/java/com/yupi/project/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.project.mapper;
+
+import com.yupi.project.model.entity.InterfaceInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Lenovo
+* @description 针对表【interface_info(接口信息)】的数据库操作Mapper
+* @createDate 2024-01-24 16:01:06
+* @Entity com.yupi.project.model.entity.InterfaceInfo
+*/
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+}
+
+
+
+
---
+++ rayapi-backend/src/main/java/com/yupi/project/service/InterfaceInfoService.java
@@ -0,0 +1,13 @@
+package com.yupi.project.service;
+
+import com.yupi.project.model.entity.InterfaceInfo;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author Lenovo
+* @description 针对表【interface_info(接口信息)】的数据库操作Service
+* @createDate 2024-01-24 16:01:06
+*/
+public interface InterfaceInfoService extends IService<InterfaceInfo> {
+    void validInterfaceInfo(InterfaceInfo interfaceInfo, boolean add);
+}
