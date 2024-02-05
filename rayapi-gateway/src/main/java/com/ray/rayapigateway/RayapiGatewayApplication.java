2d802a060be3d79f9a0b9bb4434111b52f115b72
ray <2313715475@qq.com>
2024-02-05 18:05:11 +0800

    引入gateway

---
+++ rayapi-gateway/src/main/java/com/ray/rayapigateway/RayapiGatewayApplication.java
@@ -0,0 +1,26 @@
+package com.ray.rayapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class RayapiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RayapiGatewayApplication.class, args);
+    }
+
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route", r -> r.path("/baidu")
+                        .uri("https://www.baidu.com"))
+                .route("host_route", r -> r.path("/yupiicu")
+                        .uri("http://yupi.icu"))
+                .build();
+    }
+}
---
+++ rayapi-gateway/src/test/java/com/ray/rayapigateway/RayapiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ray.rayapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RayapiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
