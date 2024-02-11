3a5f281cd2dbae36da002925aff985e53e799f07
ray <2313715475@qq.com>
2024-02-11 12:49:51 +0800

    GateWay调用公共服务

--- rayapi-gateway/src/main/java/com/ray/rayapigateway/CustomGlobalFilter.java
+++ rayapi-gateway/src/main/java/com/ray/rayapigateway/CustomGlobalFilter.java
@@ -1,7 +1,13 @@
 package com.ray.rayapigateway;
 
 import com.ray.rayapiclientsdk.utils.SignUtils;
+import com.ray.rayapicommon.model.entity.InterfaceInfo;
+import com.ray.rayapicommon.model.entity.User;
+import com.ray.rayapicommon.service.InnerInterfaceInfoService;
+import com.ray.rayapicommon.service.InnerUserInterfaceInfoService;
+import com.ray.rayapicommon.service.InnerUserService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.reactivestreams.Publisher;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
@@ -33,75 +39,86 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
     //白名单
     private static final List<String> IP_WHITE_LIST = Arrays.asList("127.0.0.1");
 
+    @DubboReference
+    private InnerUserService innerUserService;
+
+    @DubboReference
+    private InnerInterfaceInfoService innerInterfaceInfoService;
+
+    @DubboReference
+    private InnerUserInterfaceInfoService innerUserInterfaceInfoService;
+
+    private static final String INTERFACE_HOST = "http://localhost:8123";
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        //1.请求日志
+        // 1. 请求日志
         ServerHttpRequest request = exchange.getRequest();
-        log.info("请求唯一标识:" + request.getId());
-        log.info("请求路径:" + request.getPath().value());
-        log.info("请求方法" + request.getMethod());
-        log.info("请求参数" + request.getQueryParams());
+        String path = INTERFACE_HOST + request.getPath().value();
+        String method = request.getMethod().toString();
+        log.info("请求唯一标识：" + request.getId());
+        log.info("请求路径：" + path);
+        log.info("请求方法：" + method);
+        log.info("请求参数：" + request.getQueryParams());
         String sourceAddress = request.getLocalAddress().getHostString();
-        log.info("请求来源地址" + sourceAddress);
-        log.info("请求来源地址" + request.getRemoteAddress());
-
-        //2.访问控制 -黑白名单
-        //拿到响应对象
+        log.info("请求来源地址：" + sourceAddress);
+        log.info("请求来源地址：" + request.getRemoteAddress());
         ServerHttpResponse response = exchange.getResponse();
+        // 2. 访问控制 - 黑白名单
         if (!IP_WHITE_LIST.contains(sourceAddress)) {
-            // 设置响应状态码为403 Forbidden(禁止访问)
             response.setStatusCode(HttpStatus.FORBIDDEN);
-            //返回处理完成的响应
             return response.setComplete();
         }
-        // 3.用户鉴权(判断ak、sk是否合法)
-
-        // 从请求头中获取参数
+        // 3. 用户鉴权（判断 ak、sk 是否合法）
         HttpHeaders headers = request.getHeaders();
         String accessKey = headers.getFirst("accessKey");
         String nonce = headers.getFirst("nonce");
         String timestamp = headers.getFirst("timestamp");
         String sign = headers.getFirst("sign");
         String body = headers.getFirst("body");
-
         // todo 实际情况应该是去数据库中查是否已分配给用户
-
-        if (!accessKey.equals("lilray")) {
+        User invokeUser = null;
+        try {
+            invokeUser = innerUserService.getInvokeUser(accessKey);
+        } catch (Exception e) {
+            log.error("getInvokeUser error", e);
+        }
+        if (invokeUser == null) {
             return handleNoAuth(response);
         }
-        // 直接校验如果随机数大于1万，则抛出异常，并提示"无权限"
-        if (Long.parseLong(nonce) > 10000) {
+//        if (!"yupi".equals(accessKey)) {
+//            return handleNoAuth(response);
+//        }
+        if (Long.parseLong(nonce) > 10000L) {
             return handleNoAuth(response);
         }
-
-        // 时间和当前时间不能超过5分钟
-        // 首先,获取当前时间的时间戳,以秒为单位
-        // System.currentTimeMillis()返回当前时间的毫秒数，除以1000后得到当前时间的秒数。
+        // 时间和当前时间不能超过 5 分钟
         Long currentTime = System.currentTimeMillis() / 1000;
-        // 定义一个常量FIVE_MINUTES,表示五分钟的时间间隔(乘以60,将分钟转换为秒,得到五分钟的时间间隔)。
         final Long FIVE_MINUTES = 60 * 5L;
-        // 判断当前时间与传入的时间戳是否相差五分钟或以上
-        // Long.parseLong(timestamp)将传入的时间戳转换成长整型
-        // 然后计算当前时间与传入时间戳之间的差值(以秒为单位),如果差值大于等于五分钟,则返回true,否则返回false
         if ((currentTime - Long.parseLong(timestamp)) >= FIVE_MINUTES) {
-            // 如果时间戳与当前时间相差五分钟或以上，调用handleNoAuth(response)方法进行处理
             return handleNoAuth(response);
         }
-
-        // todo 实际情况中是从数据库中查出 secretKey
-        String serverSign = SignUtils.genSign(body, "abcdefgh");
-        // 如果生成的签名不一致，则抛出异常，并提示"无权限"
-        if (!sign.equals(serverSign)) {
+        // 实际情况中是从数据库中查出 secretKey
+        String secretKey = invokeUser.getSecretKey();
+        String serverSign = SignUtils.genSign(body, secretKey);
+        if (sign == null || !sign.equals(serverSign)) {
             return handleNoAuth(response);
         }
-
-        //4.判断请求的模拟接口信息是否存在
-        // todo 从数据库中查询模拟接口是否存在，以及请求方法是否匹配(还可以校验请求参数)
-
-        // 5.请求转发，调用模拟接口
-        Mono<Void> filter = chain.filter(exchange);
-        // 6.调用成功之后要输入一个响应日志
-        return handleResponse(exchange,chain);
+        // 4. 请求的模拟接口是否存在，以及请求方法是否匹配
+        InterfaceInfo interfaceInfo = null;
+        try {
+            interfaceInfo = innerInterfaceInfoService.getInterfaceInfo(path, method);
+        } catch (Exception e) {
+            log.error("getInterfaceInfo error", e);
+        }
+        if (interfaceInfo == null) {
+            return handleNoAuth(response);
+        }
+        // todo 是否还有调用次数
+        // 5. 请求转发，调用模拟接口 + 响应日志
+        //        Mono<Void> filter = chain.filter(exchange);
+        //        return filter;
+        return handleResponse(exchange, chain, interfaceInfo.getId(), invokeUser.getId());
 
     }
 
@@ -123,7 +140,12 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
                             Flux<? extends DataBuffer> fluxBody = Flux.from(body);
                             //
                             return super.writeWith(fluxBody.map(dataBuffer -> {
-                                // 7.todo 调用成功，接口调用次数 + 1  invokeCount
+                                // 7. 调用成功，接口调用次数 + 1 invokeCount
+                                try {
+                                    innerUserInterfaceInfoService.invokeCount(interfaceInfoId, userId);
+                                } catch (Exception e) {
+                                    log.error("invokeCount error", e);
+                                }
                                 byte[] content = new byte[dataBuffer.readableByteCount()];
                                 dataBuffer.read(content);
                                 DataBufferUtils.release(dataBuffer);//释放掉内存
